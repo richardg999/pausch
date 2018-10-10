d1005512de160dc9f00f0b6f188bd4982d8152ad
srilman <srinulade1@gmail.com>
2018-10-09 21:26:08 -0400

    Version 2. Using Create React App

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
-import 'react-hot-loader/patch';
-import 'babel-polyfill';
-import 'typeface-roboto';
-import './styles.css';
-
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-
+import './styles.css';
 import App from './components/App';
+import * as serviceWorker from './serviceWorker';
 
-render(
-    <AppContainer>
-        <App />
-    </AppContainer>,
-    document.getElementById('app')
-);
 
-if (module.hot) {
-    module.hot.accept(() => {
-        render(
-            <AppContainer>
-                <App />
-            </AppContainer>,
-            document.getElementById('app')
-        );
-    });
-}
+render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
